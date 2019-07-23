e74c98fa3e901baa2123b1e09879e8288d167bc0
Ondrej Chvala <ondrejch@gmail.com>
2019-07-22 22:49:00 -0400

    added water material

--- scripts/godiva.py
+++ scripts/godiva.py
@@ -31,6 +31,15 @@ mat uranium -18.74 % density 18.74 g/cm^3
 92235.03c -{enrichment}
 92238.03c -{remainder}
 
+% --- Water at 1.0 g/cm3
+%     Defined using atomic fractions for the composition. 
+%     Hydrogen is flagged as a bound scatterer with the "moder"-card
+mat water    -1.0 moder MyThermLib 1001
+ 1001.03c     2.0
+ 8016.03c     1.0
+% --- Define thermal scattering libraries associated with hydrogen in light water
+therm MyThermLib lwj3.00t
+
 %-----criticality calculation-----
 set pop 5000 120 20 % 1000 neutrons per cycle, 120 cycles, 20 of them inactive
 
@@ -42,8 +51,7 @@ set acelib "/opt/serpent/xsdata/sss_endfb7u.xsdata"
 set declib "/opt/serpent/xsdata/sss_endfb7.dec"
 set nfylib "/opt/serpent/xsdata/sss_endfb7.nfy"
 '''
-    output = output.format(**locals())
-    return output
+    output = output.format(**locals())    return output
 
 
 def runfile(filename:str='godiva.inp', queue:str='fill', cores:int=8)->str:
