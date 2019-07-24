9470cccadb73a9004b7bc40ffd37720c6df47b43
Ondrej Chvala <ondrejch@gmail.com>
2019-07-24 00:51:01 -0400

    simple flux detector

--- dev/godiva.py
+++ dev/godiva.py
@@ -6,19 +6,25 @@ Created on Sun Jul 21 11:58:42 2019
 @author: agoluogl, ondrejch
 """
 
-def deck(e:float = 93.0, r:float = 8.7407)->str:
+def deck(e:float = 58.0, r:float = 8.7407)->str:
     '''Function to write the Godiva deck.
     Inputs:
-        e:  enrichment [%]
+        e:  enrichment [%], 58% in eater tank, 93% unreflected
         r:  radius [cm]
     Outputs:
         output: String containing the Godiva deck for Serpent'''
 
-    radius     = r          # sphere radius
-    cylradius  = r*3.0      # water tank cylinder radius
-    cylhheight = r*2.0      # water tank half height
-    enrichment = e/100.0    # enrichment as a weight fraction
-    remainder  = 1.0 - enrichment 
+    do_plots:bool    = True
+    do_fluxes:bool   = False %True
+    radius:float     = r          # sphere radius
+    cylradius:float  = r*3.0      # water tank cylinder radius
+    cylhheight:float = r*2.0      # water tank half height
+    enrichment:float = e/100.0    # enrichment as a weight fraction
+    remainder:float  = 1.0 - enrichment 
+    Npercycle:int    = 5000       # neutrons per cycle
+    if do_fluxes:
+        Npercycle   *= 50         # more neutrons if we plot fluxes
+
     output ='''set title "Godiva enrichment = {e} %, radius = {radius} cm"
 
 %-----define the sphere's surface-----
@@ -47,17 +53,43 @@ mat water    -1.0 moder MyThermLib 1001  rgb 10 250 10
 therm MyThermLib lwj3.00t
 
 %-----criticality calculation-----
-set pop 5000 120 20 % 1000 neutrons per cycle, 120 cycles, 20 of them inactive
-
-%-----plot geometry-----
-% plot 2 200 200
-% plot 3 200 200
+set pop {Npercycle} 120 20 % neutrons per cycle, 120 cycles, 20 of them inactive
 
 %-----set xs library locations-----
 set acelib "/opt/serpent/xsdata/sss_endfb7u.xsdata"
 set declib "/opt/serpent/xsdata/sss_endfb7.dec"
 set nfylib "/opt/serpent/xsdata/sss_endfb7.nfy"
+'
 '''
+    # Plots
+    plot_cards = '''
+% Plots 
+plot 2 3000 2000
+plot 3 3000 3000
+mesh 2 3000 2000
+mesh 3 3000 3000
+
+'''
+    if do_plots:
+        output += plot_cards
+
+    # XY neutron flux plots - commented out by default
+    xsteps:int = 100
+    ddh:float  = cylradius / (2.0*xsteps)
+    detV:float = 2.0*ddh * 2.0*ddh * cylradius/xsteps
+    flux_xy_cards = '''
+% Flux along x - commented out by default
+% Energy grid 
+ene groupE 1 1E-11 6.25e-7 0.05 20
+% Define mesh cell volume 
+det mydet de groupE dv {detV}
+dx 0 {cylradius} {xsteps}
+dy -{ddh} {ddh} 1
+dz -{ddh} {ddh} 1
+'''
+    if do_fluxes:
+        output += flux_xy_cards
+
     output = output.format(**locals())    
     return output
 
@@ -91,8 +123,8 @@ awk 'BEGIN{{ORS="\\t"}} /ANA_KEFF/ || /CONVERSION/ {{print $7" "$8;}}' {filename
 
 
 if __name__ == '__main__':
-    print("This module writes the deck for Godiva Serpent deck.")
+    print("This module writes the deck & runfile for Godiva Serpent.")
     input("Press Ctrl+C to quit, or enter else to test it. ")
-    #print(deck())
-    print(runfile())
+    print("*** DECK ***\n"+deck())
+    print("*** RUNFILE ***\n"+runfile())
 
--- dev/writecore.py
+++ dev/writecore.py
@@ -16,8 +16,8 @@ dirname  = "./"
 parser = argparse.ArgumentParser(description='Writes Serpent2 input deck for the Godiva reactor.')
 parser.add_argument('--radius', metavar='r', type=float, nargs='?', default=8.7407,
                    help='core radius [cm], default = 8.7407 cm')
-parser.add_argument('--enrichment', metavar='e', type=float, nargs='?', default=93.0,
-                   help='fuel enrichment [%], default = 93.0%')
+parser.add_argument('--enrichment', metavar='e', type=float, nargs='?', default=58.0,
+                   help='fuel enrichment [%], default = 58.0%')
 #parser.add_argument('--ports', metavar='Nports', type=int, nargs='?', default=0, 
 #                   help='number of beam ports [0-3], default = 0', choices=[0, 1, 2, 3])
 #parser.add_argument('--rport', metavar='rport', type=float, nargs='?', default=2.0,
@@ -36,7 +36,7 @@ e_fuel  = args['enrichment']
 s2_deck = godiva.deck(r_fuel, e_fuel)
 fname   = dirname + filename
 print("Writing Godiva deck for core radius ",r_fuel," cm, enrichment ",
-    e_fuel, " %.")
+    e_fuel, "%.")
 
 # Write the deck
 try:
